97ebbacf2a5d29613dca5ae97c64d72deeebac0e
BenjaminHung8405 <nphung8405@gmail.com>
2025-11-02 21:57:10 +0700

    feat(ui): improve book card appearance
    - Added auto-ellipsis for long text
    - Adjusted label positions
    - Added tooltips for title and author

--- BookManagement/book-management/book-management/UI/Controls/ucBookCard.Designer.cs
+++ BookManagement/book-management/book-management/UI/Controls/ucBookCard.Designer.cs
@@ -47,40 +47,39 @@
             // 
             // lbTenSach
             // 
-            this.lbTenSach.AutoSize = true;
+            this.lbTenSach.AutoEllipsis = true;
             this.lbTenSach.Dock = System.Windows.Forms.DockStyle.Top;
             this.lbTenSach.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbTenSach.Location = new System.Drawing.Point(0, 160);
             this.lbTenSach.Name = "lbTenSach";
-            this.lbTenSach.Padding = new System.Windows.Forms.Padding(0, 4, 0, 0);
-            this.lbTenSach.Size = new System.Drawing.Size(60, 28);
+            this.lbTenSach.Padding = new System.Windows.Forms.Padding(4, 4, 4, 0);
+            this.lbTenSach.Size = new System.Drawing.Size(173, 48); // Giảm chiều cao từ 56 xuống 48
             this.lbTenSach.TabIndex = 1;
-            this.lbTenSach.Text = "label1";
+            this.lbTenSach.Text = "Tên sách rất dài để kiểm tra chức năng tự động rút gọn";
             // 
             // lbTacGia
             // 
-            this.lbTacGia.AutoSize = true;
+            this.lbTacGia.AutoEllipsis = true;
             this.lbTacGia.Dock = System.Windows.Forms.DockStyle.Top;
             this.lbTacGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTacGia.Location = new System.Drawing.Point(0, 188);
+            this.lbTacGia.Location = new System.Drawing.Point(0, 208); // Cập nhật vị trí
             this.lbTacGia.Name = "lbTacGia";
-            this.lbTacGia.Size = new System.Drawing.Size(51, 20);
+            this.lbTacGia.Padding = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTacGia.Size = new System.Drawing.Size(173, 24); // Giảm chiều cao từ 28 xuống 24
             this.lbTacGia.TabIndex = 1;
-            this.lbTacGia.Text = "label1";
+            this.lbTacGia.Text = "Tên tác giả";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(70)))), ((int)(((byte)(229)))));
-            this.label1.Location = new System.Drawing.Point(0, 208);
+            this.label1.Location = new System.Drawing.Point(0, 232); // Cập nhật vị trí
             this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(0, 8, 0, 0);
-            this.label1.Size = new System.Drawing.Size(51, 28);
+            this.label1.Padding = new System.Windows.Forms.Padding(4, 4, 0, 0); // Giảm padding top
+            this.label1.Size = new System.Drawing.Size(173, 28); // Cập nhật Size
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label1.Text = "Giá tiền";
             // 
             // ucBookCard
             // 
@@ -97,10 +96,9 @@
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
             this.Margin = new System.Windows.Forms.Padding(8);
             this.Name = "ucBookCard";
-            this.Size = new System.Drawing.Size(173, 248);
+            this.Size = new System.Drawing.Size(173, 260); // Tăng chiều cao tổng thể
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
--- BookManagement/book-management/book-management/UI/Controls/ucBookCard.cs
+++ BookManagement/book-management/book-management/UI/Controls/ucBookCard.cs
@@ -12,16 +12,37 @@ namespace book_management.UI.Controls
 {
     public partial class ucBookCard : UserControl
     {
+        private ToolTip toolTip;
+
         public ucBookCard()
         {
             InitializeComponent();
+            
+            // Khởi tạo ToolTip
+            toolTip = new ToolTip();
+            toolTip.AutoPopDelay = 5000; // Thời gian hiển thị tooltip
+            toolTip.InitialDelay = 500;  // Độ trễ trước khi hiển thị
+            toolTip.ReshowDelay = 500;
+            toolTip.ShowAlways = true;
+
+            // *** GIẢI PHÁP: Chuyển tiếp sự kiện Click từ control con sang control cha ***
+            this.pictureBox1.Click += (s, e) => this.OnClick(e);
+            this.lbTenSach.Click += (s, e) => this.OnClick(e);
+            this.lbTacGia.Click += (s, e) => this.OnClick(e);
+            this.label1.Click += (s, e) => this.OnClick(e);
         }
 
         // Simple helper to populate card UI
         public void SetBookData(string title, string author, decimal price, string coverUrl)
         {
+            // Gán text và tooltip cho tên sách
             lbTenSach.Text = title;
+            toolTip.SetToolTip(lbTenSach, title);
+
+            // Gán text và tooltip cho tác giả
             lbTacGia.Text = author;
+            toolTip.SetToolTip(lbTacGia, author);
+
             label1.Text = price.ToString("N0") + "₫";
 
             if (!string.IsNullOrEmpty(coverUrl))
